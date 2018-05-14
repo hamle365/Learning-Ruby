1918a3b5fc5d95585989f0ee235ebe74bd884adf
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-05-14 13:09:08 +0200

    Learn Ruby the Hard Way exercises

---
+++ ex3.rb
@@ -0,0 +1,32 @@
+# This prints a string of text that is within the ""
+puts "I will now count my chickens:"
+
+# This prints a string that has a computation within the {}
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# This prints a string of text that is within the ""
+puts "Now I will count the eggs:"
+
+# This prints the result of a calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This prints a string of text that is within the ""
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This calculates the result and prints "true" or "false"
+puts 3 + 2 < 5 - 7
+
+# This prints a string that has a computation within the {}
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+# This prints a string of text that is within the ""
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# This prints a string that has a computation within the {}, which presents a "true" or "false" result
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
