03cff1a463271eb04fcbe8d7fb32849d479e5afd
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-05-16 13:05:36 +0200

    Added comments to exercise 6

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,36 @@
+# This line creates a variable called types_of_people and assigns it the value of 10
 types_of_people = 10
+
+# This line creates a variable called x, which is assigned a string with a varible inside
 x = "There are #{types_of_people} types of people."
+
+# These lines creates a varible and assign them strings
 binary = "binary"
 do_not = "don't"
+
+# This line creates a varible called y and assigns it a string containing two other variables
 y = "Those who know #{binary} and those who #{do_not}."
 
+# These lines print the variables x and y
 puts x
 puts y
 
+# These lines print two strings, containing the variables x and y
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
+# This line creates a variable and assigns it the value of false
 hilarious = false
+
+# Ths line creates a variable and assigns it a string containing a variable
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# This line prints a variable
 puts joke_evaluation
 
+# These lines create two variables containing strings
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# This line prints the variables w and e
 puts w + e
