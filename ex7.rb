1d22a3bbe43e1748804592f3702a038f745cf561
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-06-15 11:12:22 +0200

    Exercise 7 of Learn Ruby the Hard Way

---
+++ ex7.rb
@@ -0,0 +1,36 @@
+# prints a string of text and then a new line
+puts "Mary had a little lamb."
+
+# prints a string of text with an interpolated variable and then a new line
+puts "Its fleece was white as #{'snow'}."
+
+# prints a string of text and then a new line
+puts "And everywhere that Mary went."
+
+# prints a string of text and repeats it 10 times
+puts "." * 10 # what'd that do?
+
+# creates a new variable containing a string of text
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+
+# prints all of the  below variables without creating a new line
+print end1 + end2 + end3 + end4 + end5 + end6
+
+# prints all of the below variables and creates a new line
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# I made a mistake the first time I wrote this, because I wrote "end 12" instead of "end12"
+# Variable names cannot have spaces in them
