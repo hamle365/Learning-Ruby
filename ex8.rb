0f8b71f7fb13c5ac7a3b567cafe6dd3b49e4ece3
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-06-19 13:21:56 +0200

    Added comments to exercise 8.

--- ex8.rb
+++ ex8.rb
@@ -1,10 +1,40 @@
+# This line creates a variable called formatter and assigns it a string
 formatter = "%{first} %{second} %{third} %{fourth}"
 
-puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
-puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
-puts formatter % {first: true, second: false, third: true, fourth: false}
-puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+# These lines assign values to four names, and prints them in the order set by the formatter variable
+# and then creates a new line
 
+puts formatter % {
+	first: 1,
+	second: 2,
+	third: 3,
+	fourth: 4
+}
+
+puts formatter % {
+	first: "one",
+	second: "two",
+	third: "three",
+	fourth: "four"
+}
+
+puts formatter % {
+	first: true,
+	second: false,
+	third: true,
+	fourth: false
+}
+
+# This line assigns the formatter variable to four names, and prints the formatter variable four times
+# and then creates a new line
+puts formatter % {
+	first: formatter,
+	second: formatter,
+	third: formatter,
+	fourth: formatter
+}
+
+# This line assings strings to four names, and then prints them in the order set by the formatter variable
 puts formatter % {
 	first: "I had this thing.",
 	second: "That you could type up right.",
