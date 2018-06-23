b6ca767ff94222e7ffcd96ded6ec4b670572929e
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-06-23 15:16:36 +0200

    Returns 10 percent of the number as a floating point number

---
+++ 10percent.rb
@@ -0,0 +1,8 @@
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+tenpercent = number * 0.10
+puts "10 percent is #{tenpercent}."
