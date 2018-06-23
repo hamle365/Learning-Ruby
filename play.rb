a34574beaa8d5f917d8512440902c92e14a9d1b7
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-06-23 15:51:31 +0200

    Playing with numbers and printing

---
+++ play.rb
@@ -0,0 +1,36 @@
+puts "How old is Arthur?"
+
+arthur_age = gets.chomp.to_i
+
+puts "Arthur is #{arthur_age} years old."
+
+puts "Arthur is %{age} years old." % {age: arthur_age}
+
+puts "How old is Arthur exactly?"
+
+arthur_agef = gets.chomp.to_f
+
+puts "Arthur is #{arthur_agef} years old."
+
+puts "Arthur is %.2f years old" % arthur_agef
+
+puts "Arthur is getting older, what do you think about that?"
+
+comment = gets
+
+puts " - - - - - - -
+" * 5
+
+puts "Arthur is now very old."
+
+arthur_age_old = 192
+
+puts "Arthur is so old that he is #{arthur_age_old} years old."
+
+puts "I'll tell you what 12 percent of Arthur's age is"
+
+gets
+
+arthur_10_percent = arthur_age_old * 0.12
+
+puts "12 percent of Arthur's age is %.2f years" % arthur_10_percent
