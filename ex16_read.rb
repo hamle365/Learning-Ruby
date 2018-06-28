3d2f1c2e8554eac73998573f7165b7d27613ebe1
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-06-28 12:30:13 +0200

    Exercise 16 reading script

---
+++ ex16_read.rb
@@ -0,0 +1,13 @@
+filename = ARGV.first
+
+puts "We are going to read #{filename}."
+puts "If you want to read it, hit RETURN."
+puts "If you don't want that, hit CTRL-C."
+
+$stdin.gets
+
+puts "Reading the file..."
+
+target = filename.read
+
+print target
