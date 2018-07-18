ab6666a1a9bd4ba6583709e42a94ba62c4b2c5d1
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-07-18 13:45:39 +0200

    Type: Added code Change: Added new option on lines 10, 19-34.

--- Exercises/ex31.rb
+++ Exercises/ex31.rb
@@ -7,6 +7,7 @@ if door == "1"
 	puts "There's a giant bear here eating a cheese cake. What do you do?"
 	puts "1. Take the cake."
 	puts "2. Scream at the bear."
+	puts "3. Ask the bear politely for a piece."
 
 	print "> "
 	bear = $stdin.gets.chomp
@@ -15,6 +16,22 @@ if door == "1"
 		puts "The bear eats your face off. Good job!"
 	elsif bear == "2"
 		puts "The bear eats your legs off. Good job!"
+	elsif bear == "3"
+		puts "The bear smiles and gives you a piece. You are happy. What do you say?"
+		puts "1. Thanks bear, can I go with you?"
+		puts "2. Is there more cake?"
+
+		print "> "
+		cake = $stdin.gets.chomp
+
+		if cake == "1"
+			puts "The bear snarls and motions with its paw for you to follow."
+		elsif cake == "2"
+			puts "The bear snarls loudly and bites you. You bleed to death."
+		else
+			puts "The bear doesn't like that. It attacks you and you die."
+		end
+
 	else
 		puts "What? %s?? Well, that is probably better. Bear runs away." % bear
 	end
