34148c8c2ef77f07760009e54de283b63a8287d5
Leah Hamilton <nzleah.hamilton@gmail.com>
2018-07-22 13:20:25 +0200

    Changed entire file. Because tutorial said so.

--- Exercises/ex33.rb
+++ Exercises/ex33.rb
@@ -1,16 +1,30 @@
-i = 0
-numbers = []
+def loop
+	i = 0
+	numbers = []
+	print "What number should we start with?
+> "
+	number = gets.chomp.to_i
 
-while i < 6
-	puts "At the top i is #{i}"
-	numbers.push(i)
+	print "How much do you want to increment by?
+> "
+	change = gets.chomp.to_i
 
-	i += 1
-	puts "Numbers now: ", numbers
-	puts "At the bottom i is #{i}"
+	while i < number
+		puts "At the top i is #{i}"
+
+		numbers.push(i)
+		i += change
+
+		puts "Numbers now: ", numbers
+		puts "At the bottom i is #{i}"
+
+	end
+
+	puts "The numbers: "
+	numbers.each {|num| puts num }
+
 end
 
-puts "The numbers: "
+loop
 
 # remember you can write this 2 other ways?
-numbers.each {|num| puts num }
